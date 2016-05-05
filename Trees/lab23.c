28cd8cd08c4cbfa81ee56a067f37285889c8819b
nik <nikita@nikita.io>
2016-05-06 00:00:16 +0300

    Tree update

--- Trees/lab23.c
+++ Trees/lab23.c
@@ -4,17 +4,87 @@
 #include "lab23.h"
 
 int main()
-{
+{tree t;
     srand(time(NULL));
     char act[20];
     while(1)
-    {
+    {printf("\n\n");
+        menu();
         scanf("%s",act);
-        if(!c(act)) {printf("Incorrect input, use numbers [1..8]"); continue;}
+        if(!check_menu(act)) {printf("Incorrect input, use numbers [1..8]"); continue;}
         if(act[0]=='1')
         {
-            printf("Maximum number sons in each node: 15.");
-            printf("Path to node: example: 324 thats mean than you need firstly choose 3rd son of root, than second son of current node, and, finally, choose 4th son and delete it or add brother to it");
+            printf("Maximum number sons in each node: 10.\n");
+            printf("Path to node: example: 324 thats mean than you need firstly choose 3rd son of root, than second son of current node, and, finally, choose 4th son and delete it or add brother to it\n");
+        }
+
+        if(act[0]=='2')
+        {
+            int l;
+            printf("How much levels in tree do you want?[1..15]\n");
+            scanf("%d",&l);
+            t=creation(l);
+            printf("Process sucsessfully endend\n");
+        }
+
+        if(act[0]=='3')
+        {
+            print(t);
+        }
+
+        if(act[0]=='4')
+        {
+            printf("Enter path to node(Use help for more information):\n");
+            char path[20];
+            scanf("%s",path);
+            new_node(t,path,1);
+            printf("Process sucsessfully endend\n");
+        }
+
+        if(act[0]=='5')
+        {
+            printf("Enter path to node(Use help for more information):\n");
+            char path[20];
+            scanf("%s",path);
+            new_node(t,path,0);
+            printf("Process sucsessfully endend\n");
+        }
+
+        if(act[0]=='6')
+        {
+            printf("Enter path to node(Use help for more information):\n");
+            char path[20];
+            scanf("%s",path);
+            del_subtree(t,path);
+            printf("Process sucsessfully endend\n");
+        }
+
+        if(act[0]=='7')
+        {
+            printf("Are you sure you want to delete tree?[Y/n]\n");
+            char x[3];
+            scanf("%s",x);
+            if(x[0]=='n' || x[0]=='N') continue;
+            t=del_tree(t);
+            printf("Process sucsessfully endend\n");
+
+        }
+
+        if(act[0]=='8')
+        {
+            result=0;
+            int a=task(t);
+            printf("There is(are) %d node(s) in the tree\n",a );
+        }
+
+        if(act[0]=='9')
+        {
+            printf("Sure you want to quit?[Y/n]\n");
+            char x[3];
+            scanf("%s",x);
+            if(x[0]=='n' || x[0]=='N') continue;
+            printf("Bye!\n");
+            return 0;
         }
     }
 }
--- Trees/lab23.h
+++ Trees/lab23.h
@@ -18,7 +18,6 @@ typedef struct list
 
 typedef struct node* tree;
 
-tree t;
 int levels[16];
 
 int has_sons(tree t)
@@ -38,7 +37,8 @@ int sons(tree t)
             help=help->next;
             res++;
         }
-        return res++;
+        res++;
+        return res;
     }
     return res;
 }
@@ -66,20 +66,20 @@ int length(char* a)
 }
 
 tree creation(int lvls)
-{
-    srand(time(NULL));
+{lvls--;
+    //srand(time(NULL));
     tree t=(node*)malloc(sizeof(node));
-    t->data=(float)((rand()%201)/rand()%10+11.0);
+    t->data=((float)(rand()%201))/(rand()%10+11.0);
     t->sons=NULL;
-    int s=1+rand()%4;
+    int s=1+(rand()%3);
     list* help=t->sons;
-    lvls--;
+
     if(lvls>0)
     {
         list* l=(list*)malloc(sizeof(list));
         node* new=(node*)malloc(sizeof(node));
         l->value=new;
-        (l->value)->data=(float)((rand()%201)/rand()%10+11.0);
+        (l->value)->data=((float)(rand()%201))/(rand()%10+11.0);
         (l->value)->sons=NULL;
         l->next=NULL;
         help=l;
@@ -87,15 +87,15 @@ tree creation(int lvls)
         help->value=creation(lvls);
 
 
-        for(int i=0;i<s-1;i++)
+        for(int i=0;i<s;i++)
         {
-            list* l=(list*)malloc(sizeof(list));
+            list* ll=(list*)malloc(sizeof(list));
             node* new=(node*)malloc(sizeof(node));
-            l->value=new;
-            (l->value)->data=(float)((rand()%201)/rand()%10+11.0);
-            (l->value)->sons=NULL;
-            l->next=NULL;
-            l->next=l;
+            ll->value=new;
+            (ll->value)->data=(float)((rand()%201)/rand()%10+11.0);
+            (ll->value)->sons=NULL;
+            ll->next=NULL;
+            l->next=ll;
             l=l->next;
             l->value=creation(lvls);
         }
@@ -108,8 +108,9 @@ void print(tree t)
     if(tree_empty(t)) printf("Tree is empty\n");
     else
     {
-        for(int i=0;i<=cnt;i++) printf(" ");
-        printf("%f\n",t->data);
+        for(int i=0;i<=cnt;i++) printf("  ");
+
+        printf("%d %f\n",cnt,t->data);
         if(has_sons(t))
         {
             list* help=t->sons;
@@ -117,41 +118,48 @@ void print(tree t)
             {
                 cnt++;
                 print(help->value);
+                help=help->next;
                 cnt--;
             }
+            cnt++;
+            print(help->value);
+            cnt--;
         }
     }
 }
 
 void new_node(tree t, char* path,int random)
 {
-    int ns=sons(t),n=path[cnt]-'0';
-    if(n>ns || n==0 || path[cnt]=='\0')
+    int ns=sons(t), n=path[cnt]-'0';
+    if(n>ns|| n==0 || path[cnt]=='\0')
     {
         if(t->sons)
         {
             list* help=t->sons;
+
             while(help->next!=NULL) help=help->next;
 
             list* l=(list*)malloc(sizeof(list));
             node* new=(node*)malloc(sizeof(node));
-
             l->value=new;
-            if(random) (l->value)->data=(float)((rand()%201)/rand()%10+11.0);
-            else scanf("%f",&l->value->data);
-            (l->value)->sons=NULL;
+
+            if(random) l->value->data=((float)(rand()%201))/(rand()%10+11.0);
+            else {printf("Enter value:\n"); scanf("%f",&l->value->data);}
+            l->value->sons=NULL;
             l->next=NULL;
             help->next=l;
         }
         else
         {
-            list* l=(list*)malloc(sizeof(list));
-            node* new=(node*)malloc(sizeof(node));
-            l->value=new;
-            if(random) (l->value)->data=(float)((rand()%201)/rand()%10+11.0);
-            else scanf("%f",&l->value->data);
-            (l->value)->sons=NULL;
-            t->sons=l;
+            list* help=(list*)malloc(sizeof(list));
+            node* new=(node*) malloc(sizeof(node));
+            help->value=new;
+
+            if(random) help->value->data=((float)(rand()%201))/(rand()%10+11.0);
+            else{printf("Enter value:\n"); scanf("%f",&help->value->data);}
+            help->value->sons=NULL;
+            help->next=NULL;
+            t->sons=help;
         }
     }
     else
@@ -168,14 +176,14 @@ void new_node(tree t, char* path,int random)
         {
             list* l=(list*)malloc(sizeof(list));
             node* new=(node*)malloc(sizeof(node));
-
             l->value=new;
-            if(random) (l->value)->data=(float)((rand()%201)/rand()%10+11.0);
-            else scanf("%f",&l->value->data);
+
+            if(random) l->value->data=((float)(rand()%201))/(rand()%10+11.0);
+            else {printf("Enter value:\n"); scanf("%f",&l->value->data);}
+
             l->value->sons=NULL;
             l->next=NULL;
             t->sons=l;
-
         }
     }
 }
@@ -188,7 +196,8 @@ void del_sons(tree t)
         int j=sons(t);
         for(int i=0;i<j-1;i++)
         {
-            del_sons(help->value);
+            tree tr=help->value;
+            del_sons(tr);
             help=help->next;
         }
     }
@@ -204,8 +213,8 @@ tree del_tree(tree t)
 int z=0;
 void del_subtree(tree t, char* node )
 {
-    if(!length(node) || check(node))
-        printf("Error, wrong path, try again or use help(point * of menu)");
+    if(length(node)==0 || check(node)==0)
+        printf("Error, wrong path, try again or use help(point 1 of menu)");
     else
     {
         int son=node[cnt]-'0';z++;
@@ -221,7 +230,7 @@ void del_subtree(tree t, char* node )
                     if(sons(t)==1)
                     {
                         t->sons=NULL;
-                        del_sons(help=help->value);
+                        del_sons(help->value);
                     }
                     else
                     {
@@ -231,7 +240,7 @@ void del_subtree(tree t, char* node )
                 }
                 else
                 {
-                    for(int i=0; i<num-2;i++) help=help->next;
+                    for(int i=0; i<son-2;i++) help=help->next;
 
                     list* delete=help->next;
                     help->next=delete->next;
@@ -242,8 +251,8 @@ void del_subtree(tree t, char* node )
             else
             {
                 cnt++;
-                for(int i=0;i<num-1;i++) help=help->next;
-                del_node_and_sons(help->n,path);
+                for(int i=0;i<son-1;i++) help=help->next;
+                del_subtree(help->value,node);
                 cnt--;
             }
         }
@@ -255,17 +264,41 @@ void menu()
     printf("_________________Menu_________________\n");
     printf("1.  Help\n");
     printf("2.  Tree creation\n");
-    printf("3.  Add node with random value\n");
-    printf("4.  Add node with your value\n");
-    printf("5.  Delete node and its descendants\n");
-    printf("6.  Delete whole tree\n");
-    printf("7.  Number of nodes in tree (task 22)\n");
-    printf("8.  Quit\n");
+    printf("3.  Print tree\n");
+    printf("4.  Add node with random value\n");
+    printf("5.  Add node with your value\n");
+    printf("6.  Delete node and its descendants\n");
+    printf("7.  Delete whole tree\n");
+    printf("8.  Number of nodes in tree (task 22)\n");
+    printf("9.  Quit\n");
 }
 
-int c(char* action)
+int check_menu(char* action)
 {
-    if action[1]!='\0' return 0;
-    if(action[0]>='1' && action[0]<='8') return 1;
+    if(action[0]>='1' && action[0]<='9' && action[1]=='\0') return 1;
     return 0;
+}
+int result=0;
+int task(tree t)
+{
+    if(tree_empty(t)) return 0;
+    else
+    {
+        result++;
+        if(has_sons(t))
+        {
+            list* help=t->sons;
+            while(help->next!=NULL)
+            {
+                cnt++;
+                task(help->value);
+                help=help->next;
+                cnt--;
+            }
+            cnt++;
+            task(help->value);
+            cnt--;
+        }
+    }
+    return result;
 }
